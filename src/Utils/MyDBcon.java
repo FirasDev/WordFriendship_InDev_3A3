bca71a52aab50aa9070894a9f168080ed5afcbb0
Firas Ben Mbarek <firasbenmbarek@outlook.com>
2019-02-09 14:56:03 +0100

    squelette du projet

---
+++ src/Utils/MyDBcon.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Firas
+ */
+public class MyDBcon {
+
+    private Connection con;
+    private static MyDBcon instance;
+    final String url = "jdbc:mysql://localhost:3306/wordfriendship";
+    final String user = "root";
+    final String pass = "root";
+
+    private MyDBcon() throws SQLException {
+        try {
+            con = DriverManager.getConnection(url, user, pass);
+        } catch (SQLDataException ex) {
+            Logger.getLogger(MyDBcon.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public static MyDBcon getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new MyDBcon();
+        }
+
+        return instance;
+    }
+
+}
